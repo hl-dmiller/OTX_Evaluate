1f80f7edf48fd27982248ff29e5d178285c2b805
Dusty Miller <dmiller@hurricanelabs.com>
2021-02-07 09:13:06 -0500

    Update otx_date.py with logging to an external log.

--- otx_date.py
+++ otx_date.py
@@ -1,17 +1,17 @@
-import argparse, json, urllib.request, yaml
+import argparse, json, logging, urllib.request, yaml
 from datetime import datetime,timedelta
 
 
 def pull_new_otx_iocs():
-    with open("api.yml") as api_file:
+    with open('api.yml') as api_file:
         api_keys = yaml.load(api_file, Loader=yaml.FullLoader)
-    url="https://otx.alienvault.com:443/api/v1/pulses/subscribed"
+    url='https://otx.alienvault.com:443/api/v1/pulses/subscribed'
     headers={'X-OTX-API-KEY': api_keys['otx']}
     req = urllib.request.Request(url, headers=headers)
 
     with urllib.request.urlopen(req) as data:
         otx_pull = json.loads(data.read().decode())
-        otx_pull_file_name = "otx_files/otx-" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + ".json"
+        otx_pull_file_name = 'otx_files/otx-' + datetime.now().strftime('%d-%m-%Y-%H-%M-%S') + '.json'
         write_file(otx_pull_file_name, otx_pull)
     
     return otx_pull
@@ -29,19 +29,24 @@ def write_file(out_file_name, out_file_json):
 
 
 def parse_data(otx_entry):
-    iocs_removed = 0
     results = otx_entry['results']
     for result in results[:]:
         indicators = result['indicators']
-        print("{} has {} indicators".format(result['id'],len(indicators)))
+        iocs_removed = 0
+        iocs_added = 0
         for indicator in indicators[:]:
             if compare_date(indicator['created']) > timedelta(days=7):
                 result['indicators'].remove(indicator)
                 iocs_removed += 1
+            else:
+                iocs_added +=1
+        if iocs_removed > 0:
+            logging.info('OTX Pulse {} - "{}" - Removed {} IOCs'.format(result['id'],result['name'],iocs_removed))
+        else:
+            logging.info('OTX Pulse {} - "{}" - Added {} IOCs'.format(result['id'],result['name'],iocs_added))
         if not result['indicators']:
             otx_entry['results'].remove(result)
-            print("Removed {} from OTX pull".format(result['name']))
-    print("Removed {} IOCs from OTX pull.".format(iocs_removed))
+            logging.info('OTX Pulse {} - "{}" - Removed full Pulse'.format(result['id'],result['name']))
     return otx_entry
 
 
@@ -53,9 +58,11 @@ def compare_date(ioc_date):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("out_file", help="Print to file (provide file name)")
-    parser.add_argument("-t", "--test_file", help="Run against test file - this will prevent pull of new IOCs")
+    parser.add_argument('out_file', help='Print to file (provide file name)')
+    parser.add_argument('-t', '--test_file', help='Run against test file - this will prevent pull of new IOCs')
     args = parser.parse_args()
+
+    logging.basicConfig(filename='otx.log', filemode='a', level=logging.INFO, format='%(asctime)s - %(message)s')
 
     if args.test_file:
         otx_test_file = get_file(args.test_file)
